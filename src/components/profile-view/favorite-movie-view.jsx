ebf93aed0d475faa145e18702c6ce63e4103b4ff
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-06-28 21:19:08 +0200

    Revert "Update favorite-movie-view.jsx"
    This reverts commit 9ff43cc0d19e0cc1357807dff72822dd93757688.

--- src/components/profile-view/favorite-movie-view.jsx
+++ src/components/profile-view/favorite-movie-view.jsx
@@ -8,16 +8,16 @@ import { Button, Card, Col } from 'react-bootstrap';
 import './profile-view.scss';
 
 export function FavoriteMoviesView(props) {
-  const { movies, FavoriteMovies, Username, token } = props;
+  const { movies, favoriteMovies, currentUser, token } = props;
 
-  const FavoriteMoviesId = FavoriteMovies.map(m => m._id)
+  const favoriteMoviesId = favoriteMovies.map(m => m._id)
 
-  const FavoriteMoviesList = movies.filter(m => {
-    return FavoriteMoviesId.includes(m._id)
+  const favoriteMoviesList = movies.filter(m => {
+    return favoriteMoviesId.includes(m._id)
   })
 
   const handleMovieDelete = (movieId) => {
-    axios.delete(`https://quiet-savannah-08380.herokuapp.com/users/${Username}/${movieId}`, {
+    axios.delete(`https://quiet-savannah-08380.herokuapp.com/users/${currentUser}/${movieId}`, {
       headers: { Authorization: `Bearer ${token}` }
     })
       .then(() => {
@@ -29,10 +29,10 @@ export function FavoriteMoviesView(props) {
 
   return (
     <Fragment>
-      {FavoriteMoviesList.length === 0 ? (
+      {favoriteMoviesList.length === 0 ? (
         <p>You have no favorite movies yet.</p>
       ) : (
-        FavoriteMoviesList.map((movie) => {
+        favoriteMoviesList.map((movie) => {
           return (
             <Col xs={10} sm={8} md={6} lg={4} >
               <Card id="movie-card">
