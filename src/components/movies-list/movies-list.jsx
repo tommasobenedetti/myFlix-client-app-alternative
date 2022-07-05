619fe5aac4f8a27c2ce3c4512cf30d22a6b2707d
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-07-05 16:24:45 +0200

    responsiveness

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -28,7 +28,7 @@ function MoviesList({ movies, visibilityFilter }) {
             </Col>
 
             {filteredMovies.map((m) => (
-                <Col sm={6} md={6} lg={4} xl={3} key={m._id}>
+                <Col sm={8} md={8} lg={5} xl={3} key={m._id}>
                     <MovieCard movie={m} />
                 </Col>
             ))}
