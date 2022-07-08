d4e906efce273f237eb9c0e726ca8cf28ee15e69
tommasobenedetti <tommasobenedetti89@gmail.com>
2022-07-08 11:47:19 +0200

    merge

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import { Button, Col, Container, Row } from 'react-bootstrap';
